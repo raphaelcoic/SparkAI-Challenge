75a9f19e84baea36a04b384aea53bb22f1acb516
Raph <raphaelcoicwulfman@gmail.com>
2026-02-14 19:31:05 +0100

    Création class worker avec fonctions move, extract, drop

---
+++ code/Units.py
@@ -0,0 +1,39 @@
+from GameMap import *
+
+class worker:
+    """Class for a worker unit."""
+    def __init__(self, team = None, position = None):
+        self.player = player
+        self.position = position
+        self.health = 5
+        self.speed = 1
+        self.extraction_speed = 1
+        self.load = 0
+        self.capacity = 3
+
+    def set_map(self, map):
+        self.map = map
+
+    def move(self, v):
+        """Moves the worker to v if it is adjacent to it."""
+        if v in self.map.neighbors(self.position):
+            self.position = v
+        else:
+            print("Impossible move")
+
+    def extract(self):
+        """Extracts resources from the map."""
+        if self.load < self.capacity and self.map.resources(self.position) != 0:
+            available_resources = self.map.resources[self.position]
+            available_space = self.capacity - self.load
+            extracted_resources = min(available_resources, available_space, self.extraction_speed)
+
+            self.load += extracted_resources
+            self.map.resources[self.position] -= extracted_resources
+
+        else:
+            print("Impossible extraction")
+
+    def drop(self):
+            self.map.resources[self.position] += self.load
+            self.load = 0
